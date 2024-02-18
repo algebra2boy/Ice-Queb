7abe1a5feee6855df14d7893d5eb08ad3d48492b
Yongye Tan <yongyetan1209@gmail.com>
2024-02-18 15:48:50 -0500

    add auth service login and sign up logic

--- src/api/services/auth/auth.controller.ts
+++ src/api/services/auth/auth.controller.ts
@@ -1,6 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
 import * as userService from './auth.service.js';
-import { HttpError } from '../../utils/httpError.util.js';
 
 export const login = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -11,13 +10,10 @@ export const login = async (req: Request, res: Response, next: NextFunction) => 
     }
 };
 
-export const signup = (req: Request, res: Response, next: NextFunction) => {
+export const signup = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        throw new HttpError(400, {
-            message: '123',
-            status: 'failure',
-        });
-        res.json({ message: 'signup' });
+        const user = await userService.signup(req.body);
+        res.status(200).json(user);
     } catch (error) {
         next(error);
     }
--- src/api/services/auth/auth.service.ts
+++ src/api/services/auth/auth.service.ts
@@ -1,19 +1,81 @@
 import { Collection } from 'mongodb';
+import status from 'http-status';
+import bcrypt from 'bcrypt';
 
-import { User } from './auth.model.js';
+import { MongoDB } from '../../configs/database.config.js';
+import { User, RegisterUser } from './auth.model.js';
+import { HttpError } from '../../utils/httpError.util.js';
+import { generateToken } from '../../utils/token.util.js';
 
-async function login(bodyPayload: User) {}
+async function login(payload: User): Promise<RegisterUser> {
+    const accountCollection: Collection<User> = MongoDB.getIceQuebDB().collection('account');
 
-/**
- * HELPER FUNCTIONS FOR THE AUTH SERVICES
- */
+    const user = await findUserByEmail(accountCollection, payload.email);
 
-async function findUserByEmail(userCollection: Collection<User>, email: string) {
-    const user: User | null = await userCollection.findOne({ email: email });
+    if (!user) {
+        throw new HttpError(status.NOT_FOUND, {
+            message: 'user does not exist in the database',
+            status: 'failure',
+        });
+    }
 
-    // if (!user) {
-    //     throw new Http
-    // }
+    const isPasswordCorrect: boolean = await validatePassword(payload.password, user.password);
+
+    if (!isPasswordCorrect) {
+        throw new HttpError(status.UNAUTHORIZED, {
+            message: 'user password is not correct',
+            status: 'failure',
+        });
+    }
+
+    return {
+        email: user.email,
+        isTeacher: user.isTeacher,
+        token: generateToken(user.email),
+    };
 }
 
-export { login };
+async function signup(payload: User): Promise<RegisterUser> {
+    const accountCollection: Collection<User> = MongoDB.getIceQuebDB().collection('account');
+
+    const existingUser = await findUserByEmail(accountCollection, payload.email);
+
+    if (existingUser) {
+        throw new HttpError(status.FORBIDDEN, {
+            message: 'user already exists in the database',
+            status: 'failure',
+        });
+    }
+
+    await createNewUser(accountCollection, payload);
+
+    return {
+        email: payload.email,
+        isTeacher: payload.isTeacher,
+        token: generateToken(payload.email),
+    };
+}
+
+//
+//  HELPER FUNCTIONS FOR THE AUTH SERVICES
+//
+
+async function findUserByEmail(accountCollection: Collection<User>, email: string) {
+    return await accountCollection.findOne({ email: email });
+}
+
+async function createNewUser(accountCollection: Collection<User>, payload: User): Promise<void> {
+    const hashedPassword = await bcrypt.hash(payload.password, 10);
+
+    await accountCollection.insertOne({
+        email: payload.email,
+        password: hashedPassword,
+        isTeacher: payload.isTeacher,
+    });
+}
+
+async function validatePassword(payloadPassword: string, hashedPassword: string): Promise<boolean> {
+    return await bcrypt.compare(payloadPassword, hashedPassword);
+}
+
+export { login, signup };
