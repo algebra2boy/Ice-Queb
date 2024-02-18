19b918d3070e84ba7b8ad3e1f9b760a96ec1d3ae
George Zhang <georgezhang744@gmail.com>
2024-02-18 13:08:54 -0500

    edit response format

--- src/api/services/auth/auth.controller.ts
+++ src/api/services/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 
 export const login = (req: Request, res: Response) => {
     res.json({ message: 'login' });
+    
 };
 
 export const signup = (req: Request, res: Response) => {
