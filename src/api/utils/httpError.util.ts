8a61aa4ebdbb474b921c97b299a2068c2f67ba6c
Yongye Tan <yongyetan1209@gmail.com>
2024-02-18 11:02:59 -0500

    add code example

--- src/api/utils/httpError.util.ts
+++ src/api/utils/httpError.util.ts
@@ -1,5 +1,17 @@
 type ErrorMessage = { message: string; status: 'failure' };
 
+/**
+ * This HttpError class standardizes our response when there is an error on the client or server side.
+ *  @example 
+ * ```ts
+ * import { HttpError } from '../../utils/httpError.util.js';
+ * if (condition) {
+ *     throw new HttpError(404, {
+ *          message: <custom-message>, status: 'failure'
+ *     });
+ * }
+ * ```
+ */
 export class HttpError extends Error {
     errorCode: number;
     public readonly error: ErrorMessage;
