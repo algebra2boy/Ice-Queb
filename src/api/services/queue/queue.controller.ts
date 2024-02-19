6c669c267b76a85d6e9425357c888c7da1d99f83
Yongye Tan <yongyetan1209@gmail.com>
2024-02-19 16:38:28 -0500

    separate queue controller and service

--- src/api/services/queue/queue.controller.ts
+++ src/api/services/queue/queue.controller.ts
@@ -1,116 +1,28 @@
-import { Request, Response } from 'express';
-import { Queue, StudentInQueue } from './queue.model.js';
-import status from 'http-status';
-import { HttpError } from '../../utils/httpError.util.js';
+import { Request, Response, NextFunction } from 'express';
+import * as queueService from "./queue.service.js";
+import status from "http-status";
 
-// For testing, will be replaced by actual database
-import fs from 'fs/promises';
-import path from 'path';
-
-const ASSETS_FOLDER = path.resolve(process.cwd(), 'src/assets');
-
-// temporary helper functions
-const loadQueues = async () => {
+export const getQueueInfo = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const data = await fs.readFile(path.resolve(ASSETS_FOLDER, 'queues.json'), 'utf8');
-        const queues = JSON.parse(data);
-        return queues;
-    } catch (err) {
-        console.log(err);
+        const queue = queueService.getQueueInfo(req.body);
+        res.status(status.OK).json(queue);
+    } catch (error) {
+        next(error);
     }
-};
-const updateQueues = async (newQueues: Queue[]) => {
+}
+
+export const joinQueue = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const data = JSON.stringify(newQueues);
-        await fs.writeFile(path.resolve(ASSETS_FOLDER, 'queues.json'), data, 'utf8');
-    } catch (err) {
-        console.log(err);
+
+    } catch (error) {
+        next(error);
     }
-};
+}
 
-const findTargetQueue = (payload: Queue, queue: Queue) => {
-    const { className, sessionNumber, day, startTime } = payload;
-    return (
-        queue.className === className &&
-        queue.sessionNumber === sessionNumber &&
-        queue.day === day &&
-        queue.startTime === startTime
-    );
-};
+export const leaveQueue = async (req: Request, res: Response, next: NextFunction) => {
+    try {
 
-// Router functions
-export const getQueueInfo = async (req: Request, res: Response) => {
-    // const { className, sessionNumber, day, startTime } = req.body;
-
-    // will be replaced by actual database
-    const queues = await loadQueues();
-
-    // const targetQueue = findTargetQueue(req.body, queue);
-
-    const targetQueue = queues.find((queue: Queue) => {
-        return findTargetQueue(req.body, queue);
-    });
-
-    console.log(targetQueue);
-
-    if (targetQueue) {
-        res.json(targetQueue);
-    } else {
-        throw new HttpError(status.NOT_FOUND, 'no such queue is found');
+    } catch (error) {
+        next(error);
     }
-};
-
-export const joinQueue = async (req: Request, res: Response) => {
-    const { studentEmail } = req.body;
-
-    // will be replaced by actual database
-    const queues = await loadQueues();
-
-    const targetQueue = queues.find((queue: Queue) => {
-        return findTargetQueue(req.body, queue);
-    });
-
-    if (targetQueue) {
-        targetQueue.studentList.push({ studentEmail, joinTime: new Date() });
-
-        // will be replaced by actual database
-        await updateQueues(queues);
-
-        res.json({ status: 'Success' });
-    } else {
-        throw new HttpError(status.NOT_FOUND, 'no such queue is found');
-    }
-};
-
-export const leaveQueue = async (req: Request, res: Response) => {
-    const { studentEmail } = req.body;
-
-    // will be replaced by actual database
-    const queues = await loadQueues();
-
-    const targetQueue = queues.find((queue: Queue) => {
-        return findTargetQueue(req.body, queue);
-    });
-
-    if (targetQueue) {
-        let targetIdx = -1;
-
-        targetQueue.studentList.forEach((student: StudentInQueue, idx: number) => {
-            if (student.email === studentEmail) {
-                targetIdx = idx;
-                return true;
-            } else {
-                return false;
-            }
-        });
-
-        targetQueue.studentList.splice(targetIdx, 1);
-
-        // will be replaced by actual database
-        await updateQueues(queues);
-
-        res.json({ status: 'Success' });
-    } else {
-        throw new HttpError(status.NOT_FOUND, 'no such queue is found');
-    }
-};
+}
---
+++ src/api/services/queue/queue.service.ts
@@ -0,0 +1,115 @@
+import { Request, Response } from 'express';
+import { Queue, StudentInQueue } from './queue.model.js';
+import status from 'http-status';
+import { HttpError } from '../../utils/httpError.util.js';
+
+// For testing, will be replaced by actual database
+import fs from 'fs/promises';
+import path from 'path';
+
+const ASSETS_FOLDER = path.resolve(process.cwd(), 'src/assets');
+
+// temporary helper functions
+const loadQueues = async () => {
+    try {
+        const data = await fs.readFile(path.resolve(ASSETS_FOLDER, 'queues.json'), 'utf8');
+        const queues = JSON.parse(data);
+        return queues;
+    } catch (err) {
+        console.log(err);
+    }
+};
+const updateQueues = async (newQueues: Queue[]) => {
+    try {
+        const data = JSON.stringify(newQueues);
+        await fs.writeFile(path.resolve(ASSETS_FOLDER, 'queues.json'), data, 'utf8');
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const findTargetQueue = (payload: Queue, queue: Queue) => {
+    const { className, sessionNumber, day, startTime } = payload;
+    return (
+        queue.className === className &&
+        queue.sessionNumber === sessionNumber &&
+        queue.day === day &&
+        queue.startTime === startTime
+    );
+};
+
+// Router functions
+export const getQueueInfo = async (queue: Queue) => {
+
+    // will be replaced by actual database
+    const queues = await loadQueues();
+
+    // const targetQueue = findTargetQueue(req.body, queue);
+
+    const targetQueue = queues.find((queue: Queue) => {
+        return findTargetQueue(queue, queue);
+    });
+
+    console.log(targetQueue);
+
+    if (targetQueue) {
+        res.json(targetQueue);
+    } else {
+        throw new HttpError(status.NOT_FOUND, 'no such queue is found');
+    }
+};
+
+export const joinQueue = async (req: Request, res: Response) => {
+    const { studentEmail } = req.body;
+
+    // will be replaced by actual database
+    const queues = await loadQueues();
+
+    const targetQueue = queues.find((queue: Queue) => {
+        return findTargetQueue(req.body, queue);
+    });
+
+    if (targetQueue) {
+        targetQueue.studentList.push({ studentEmail, joinTime: new Date() });
+
+        // will be replaced by actual database
+        await updateQueues(queues);
+
+        res.json({ status: 'Success' });
+    } else {
+        throw new HttpError(status.NOT_FOUND, 'no such queue is found');
+    }
+};
+
+export const leaveQueue = async (req: Request, res: Response) => {
+    const { studentEmail } = req.body;
+
+    // will be replaced by actual database
+    const queues = await loadQueues();
+
+    const targetQueue = queues.find((queue: Queue) => {
+        return findTargetQueue(req.body, queue);
+    });
+
+    if (targetQueue) {
+        let targetIdx = -1;
+
+        targetQueue.studentList.forEach((student: StudentInQueue, idx: number) => {
+            if (student.email === studentEmail) {
+                targetIdx = idx;
+                return true;
+            } else {
+                return false;
+            }
+        });
+
+        targetQueue.studentList.splice(targetIdx, 1);
+
+        // will be replaced by actual database
+        await updateQueues(queues);
+
+        res.json({ status: 'Success' });
+    } else {
+        throw new HttpError(status.NOT_FOUND, 'no such queue is found');
+    }
+};
