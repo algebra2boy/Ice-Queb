81c491125efb42100b8ac4fd0c711cef3ce45e66
Yongye Tan <yongyetan1209@gmail.com>
2024-02-18 19:38:27 -0500

    update status

--- src/api/services/auth/auth.controller.ts
+++ src/api/services/auth/auth.controller.ts
@@ -15,7 +15,7 @@ export const login = async (req: Request, res: Response, next: NextFunction) => 
 export const signup = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const user = await userService.signup(req.body);
-        res.status(status.OK).json(user);
+        res.status(status.CREATED).json(user);
     } catch (error) {
         next(error);
     }
