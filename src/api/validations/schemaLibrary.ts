efb751d28a1c778865ff697a90468d359c2ebf94
Yongye Tan <yongyetan1209@gmail.com>
2024-03-16 20:02:26 -0400

    add accidentally missing password requirment

--- src/api/validations/schemaLibrary.ts
+++ src/api/validations/schemaLibrary.ts
@@ -6,7 +6,8 @@ export default {
         .min(1, 'Email should have at least one character')
         .email('This is not a valid email'),
     passwordValidation: z
-        .string({ required_error: 'Password does not exist' }),
+        .string({ required_error: 'Password does not exist' })
+        .min(8, 'Password should have at least eight character'),
     classNameValidation: z
         .string({ required_error: 'Class name does not exist' })
         .min(1, 'Class name should have at least one character'),
