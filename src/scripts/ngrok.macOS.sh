b11687a1758bcd457424c74eac5cbbd88bcac40d
Yongye Tan <yongyetan1209@gmail.com>
2024-03-19 15:35:41 -0400

    update shell program

--- src/scripts/ngrok.macOS.sh
+++ src/scripts/ngrok.macOS.sh
@@ -1,6 +1,11 @@
+#!/bin/sh
+
 # You might need to "chmod +x ./ngrok.macOS.sh"
 # Then run "./ngrok.macOS.sh" to execute the script
 # "chmod +x is to add executable permission to the file"
+# MAKE SURE to export your NGROK_AUTH_TOKEN and APP_URL in .env file
+# export NGROK_AUTH_TOKEN=<token>
+# export APP_URL=<url link>
 
 # Install ngrok via homebrew if not installed
 if ! command -v ngrok &> /dev/null
@@ -9,8 +14,13 @@ then
     brew install ngrok
 fi
 
+if [ ! -f .env ]
+then
+  export $(cat .env | xargs)
+fi
+
 # Add your authtoken from .env file
 ngrok config add-authtoken $NGROK_AUTH_TOKEN
 
-# Expose a local server to the internet
+# # Expose a local server to the internet
 ngrok http $APP_URL
