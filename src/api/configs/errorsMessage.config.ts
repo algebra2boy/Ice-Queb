9bbc0fb4f856b2fdf4dad1c849ef3f27e863500f
Yongye Tan <yongyetan1209@gmail.com>
2024-03-19 11:18:31 -0400

    add jwt middleware

--- src/api/configs/errorsMessage.config.ts
+++ src/api/configs/errorsMessage.config.ts
@@ -7,7 +7,7 @@ export const ErrorMessages = {
     USER_PASSWORD_NOT_CORRECT: (email: string) => `user password is not correct for ${email}`,
     USER_ALREADY_EXISTS: (email: string) => `user with ${email} already exists in the database`,
 
-    // Class service error messages
+    // Office Hour service error messages
     OFFICE_HOUR_STUDENT_DOCUMENT_NOT_FOUND: (email: string) =>
         `There is no office hour document for ${email}`,
     STUDENT_OFFICE_HOUR_NOT_FOUND: (email: string) => `There is no office hour for ${email}`,
--- src/api/middlewares/Error.middleware.ts
+++ src/api/middlewares/Error.middleware.ts
@@ -3,13 +3,22 @@ import status from 'http-status';
 
 import { HttpError } from '../utils/httpError.util.js';
 
+interface JwtError extends Error {
+    inner: { message: string };
+}
+
 /**
  * This middleware will intercept all the possible errors from the server.
  * Instead of returning the entire error log back, we return the specific
  * message and error back to the client.
  */
 const ErrorMiddleware = (error: Error, req: Request, res: Response, next: NextFunction) => {
-    if (error && error instanceof HttpError) {
+    if (error && error.name === 'UnauthorizedError') {
+        res.status(status.UNAUTHORIZED).json({
+            message: (error as JwtError).inner.message,
+            status: 'failure',
+        });
+    } else if (error && error instanceof HttpError) {
         res.status(error.errorCode).json({
             message: error.errorMessage,
             status: 'failure',
