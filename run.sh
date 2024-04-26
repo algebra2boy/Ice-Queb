b6ea95b5bcf3e2b951b79b70981facfb3916a6f6
Kolyn090 <78561131+Kolyn090@users.noreply.github.com>
2024-04-25 23:17:45 -0400

    Added commend to shell for installing node 17.0:

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+nvm install 17
+nvm use 17
+
 # npm operation
 npm run build
 npm run start & # run in the background
