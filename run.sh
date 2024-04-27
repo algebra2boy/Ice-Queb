72c9957c6bc207b6fe95b4acb31e86209cb5fb9f
Kolyn090 <78561131+Kolyn090@users.noreply.github.com>
2024-04-26 21:28:29 -0400

    Added a kill process command to shell

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo killall -9 node # Dangerous
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
