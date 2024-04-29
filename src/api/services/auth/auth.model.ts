bc622b2e315ea6331fdf5078d965880b397dbc12
GeorgeZhang744 <georgezhang744@gmail.com>
2024-04-28 22:16:07 -0400

    Add isTeacher attribute to all User interface

--- src/api/services/auth/auth.model.ts
+++ src/api/services/auth/auth.model.ts
@@ -1,16 +1,19 @@
 export interface User {
     email: string;
     password: string;
+    isTeacher: boolean;
 }
 
 export interface RegisterUser {
     email: string;
     token: string;
     status: string;
+    isTeacher: boolean;
 }
 
 export interface ResetUser {
     email: string;
     oldPassword: string;
     newPassword: string;
+    isTeacher: boolean;
 }
