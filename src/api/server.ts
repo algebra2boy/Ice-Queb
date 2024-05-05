ab0d161e248934aa5790f6250cb11f89bfa6d0ee
Haijie <64161293+ygbull@users.noreply.github.com>
2024-05-05 00:20:21 -0400

    For server deployment

--- src/api/server.ts
+++ src/api/server.ts
@@ -5,12 +5,15 @@ import * as https from 'https';
 import * as fs from 'fs';
 
 const PORT = process.env.PORT || 3000;
-const SSL_CERT_PATH = process.env.SSL_CERT_PATH || 'localhost.crt';
-const SSL_KEY_PATH = process.env.SSL_KEY_PATH || 'localhost.key';
+const SSL_CERT_PATH = process.env.SSL_CERT_PATH || 'certificate.crt';
+const SSL_KEY_PATH = process.env.SSL_KEY_PATH || 'private.key';
+const SSL_CA_BUNDLE_PATH = process.env.SSL_CA_BUNDLE_PATH || 'ca_bundle.crt';
 
 const privateKey = fs.readFileSync(SSL_KEY_PATH, 'utf8');
 const certificate = fs.readFileSync(SSL_CERT_PATH, 'utf8');
-const credentials = { key: privateKey, cert: certificate };
+const caBundle = fs.readFileSync(SSL_CA_BUNDLE_PATH, 'utf8');
+
+const credentials = { key: privateKey, cert: certificate, ca: caBundle };
 
 MongoDB.runServer();
 
@@ -18,7 +21,7 @@ const server = https.createServer(credentials, app);
 
 server.listen(PORT, () => {
     if (process.env.NODE_ENV !== 'test') {
-        console.log(`the server is running on port ${PORT}`);
+        console.log(`The server is running on port ${PORT}`);
     }
 });
 
